de6a179f4b60d19a13aaf296c58e65dcb807d053
Alexander Vogel <alexander.vogel@prozesskraft.de>
2018-04-18 21:38:11 +0200

    implement merging tables

---
+++ data_structures/3-3_merging_tables.py
@@ -0,0 +1,53 @@
+# python3
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+lines = list(map(int, sys.stdin.readline().split()))
+rank = [1] * n
+parent = list(range(0, n))
+ans = max(lines)
+
+def getParent(table):
+    # find parent and compress path
+    if(table != parent[table]):
+        parent[table] =  getParent(parent[table])
+    return parent[table]
+
+def merge(destination, source):
+
+    global ans
+    
+    realDestination, realSource = getParent(destination), getParent(source)
+
+    if realDestination == realSource:
+        return False
+
+    # merge two components
+    # use union by rank heuristic 
+    # update ans with the new maximum table size
+    if rank[realDestination] < rank[realSource]:
+    
+        # set real destination to real source
+        parent[realDestination] = realSource
+
+        # add lines
+        lines[realSource] = lines[realSource] + lines[realDestination]
+        lines[realDestination] = 0
+
+    else:
+        parent[realSource] = realDestination
+        lines[realDestination] = lines[realDestination] + lines[realSource]
+        lines[realSource] = 0
+        if (rank[realDestination] == rank[realSource]):
+            rank[realDestination] = rank[realDestination] + 1
+
+    ans = max(ans, lines[realSource], lines[realDestination])
+
+    return True
+
+for i in range(m):
+    destination, source = map(int, sys.stdin.readline().split())
+    merge(destination - 1, source - 1)
+    print(ans)
+    
